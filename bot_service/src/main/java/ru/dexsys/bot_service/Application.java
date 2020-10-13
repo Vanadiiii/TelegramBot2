18b35f3186d75d69f923de4707c49a70f10a4835
Ivan Matveev <vanadiiii42@gmail.com>
2020-10-13 08:44:06 +0400

    [no-task] small fixes

--- bot_service/src/main/java/ru/dexsys/bot_service/Application.java
+++ bot_service/src/main/java/ru/dexsys/bot_service/Application.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 
-@SpringBootApplication(scanBasePackages = {
-        "ru.dexsys.bot_service",
-        "ru.dexsys.domain",
-        "ru.dexsys.repository"
-})
+@SpringBootApplication(scanBasePackages = "ru.dexsys")
 public class Application {
     public static void main(String[] args) {
         ApiContextInitializer.init();
