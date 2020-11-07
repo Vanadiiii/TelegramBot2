454e14cce17cd704f00815b67d16646bf06b6d18
Ivan Matveev <vanadiiii42@gmail.com>
2020-11-07 17:38:55 +0400

    [CVD3-555] small fixes

--- inner_storage/src/main/java/ru/dexsys/config/UserTempRepConfig.java
+++ inner_storage/src/main/java/ru/dexsys/config/UserTempRepConfig.java
@@ -1,11 +1,10 @@
 package ru.dexsys.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import ru.dexsys.repository.UserTempDataRepository;
 
-@EnableJpaRepositories
 @Configuration
-@PropertySource("classpath:application-dev.properties")
+@EnableJpaRepositories(basePackageClasses = UserTempDataRepository.class)
 public class UserTempRepConfig {
 }
